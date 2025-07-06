734b03e2abeae68cc3331dd3ddbaafbb1d35c3df
benson <kumadth@gmail.com>
2025-07-06 23:50:32 +0800

    conflict solved

---
+++ WebApplication1/WebApplication1/Book_LOCAL_1302.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebApplication1
+{
+    class Book
+    {
+        public string Title { get; set; }
+
+        public List<string> Authors { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        fix from Book1 branch
+    }
+}
