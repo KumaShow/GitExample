365f72d5a150df3f03d1ef12aeb1b49b2285790c
benson <kumadth@gmail.com>
2025-07-06 22:20:26 +0800

    update

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -26,32 +25,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-=======
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-// test git
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-Console.WriteLine("Hello, World!");
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
->>>>>>> 13d548d6d027793349f9daab3881f46d6a67d06b
