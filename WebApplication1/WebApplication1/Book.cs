a9b88a760e7349e3fa481daeea950713fefe7f29
benson <kumadth@gmail.com>
2025-07-06 23:39:45 +0800

    add fix from book1

--- WebApplication1/WebApplication1/Book.cs
+++ WebApplication1/WebApplication1/Book.cs
@@ -5,8 +5,11 @@ namespace WebApplication1
     class Book
     {
         public string Title { get; set; }
+
         public List<string> Authors { get; set; }
+
         public DateTime PublicationDate { get; set; }
 
+        fix from Book1 branch
     }
 }
